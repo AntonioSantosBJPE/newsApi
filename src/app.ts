a22e876c9608842cef174a534d4c23498ce20880
Antonio <netoifpe@gmail.com>
2023-03-30 21:10:27 -0300

    feat: create router /users/

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,18 @@ import "express-async-errors";
 import express, { Application, Request, Response } from "express";
 import { errorHandler } from "./errors";
 import cors from "cors";
+import { usersRoutes } from "./routers/users.routes";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/teste", async (req: Request, res: Response): Promise<Response> => {
+app.use("/teste/", async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).json("teste");
 });
+
+app.use("/users/", usersRoutes);
 
 app.use(errorHandler);
 
