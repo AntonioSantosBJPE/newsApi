e3acfb00bc805c746f939ee3edb1543681867bfd
Antonio <netoifpe@gmail.com>
2023-04-22 17:23:32 -0300

    feat: create api documentation using swagger

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,15 @@ import express, { Application } from "express";
 import { errorHandler } from "./errors";
 import cors from "cors";
 import * as routers from "./routers";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use("/api-docs/", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use("/users", routers.usersRoutes);
 app.use("/news", routers.newsRoutes);
 app.use("/tags", routers.tagsRoutes);
