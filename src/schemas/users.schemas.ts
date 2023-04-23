f5b1bfeb5adb59608a8469a6da3261992d7a7897
Antonio <netoifpe@gmail.com>
2023-04-23 09:31:58 -0300

    fix: fix user password regex in zod user schema

--- src/schemas/users.schemas.ts
+++ src/schemas/users.schemas.ts
@@ -4,24 +4,42 @@ export const UserSchema = z.object({
   id: z.number().positive().int(),
   name: z.string().max(80),
   email: z.string().email().max(80),
-  password: z.string().max(120),
+  password: z
+    .string()
+    .regex(new RegExp(".*[A-Z].*"), "One uppercase character")
+    .regex(new RegExp(".*[a-z].*"), "One lowercase character")
+    .regex(new RegExp(".*\\d.*"), "One number")
+    .regex(
+      new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),
+      "One special character"
+    )
+    .min(8)
+    .max(32),
   isAdmin: z.boolean(),
   createdAt: z.date(),
   updatedAt: z.date(),
   isDeleted: z.boolean(),
 });
 
-export const UserCreateSchema = z.object({
-  name: z.string().max(80),
-  email: z.string().email().max(80),
-  password: z.string().max(120),
-  isAdmin: z.boolean().optional().default(false),
+export const UserCreateSchema = UserSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+  isDeleted: true,
 });
 
-export const UserUpdateSchema = z.object({
-  name: z.string().max(80).optional(),
-  password: z.string().max(120).optional(),
-});
+export const UserUpdateSchema = UserSchema.omit({
+  id: true,
+  email: true,
+  isAdmin: true,
+  createdAt: true,
+  updatedAt: true,
+  isDeleted: true,
+}).partial();
+// export const UserUpdateSchema = z.object({
+//   name: z.string().max(80).optional(),
+//   password: z.string().max(120).optional(),
+// });
 
 export const UserReturnCreatedSchema = UserSchema.omit({
   password: true,
